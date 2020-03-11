6f4c68bc48ad35d4419483ed14574eafe19f69e1
RenYuZhuo <renyuzhuo@foxmail.com>
2020-03-12 02:38:19 +0800

    [feat]Update Format

--- src/pages/artical/artical.tsx
+++ src/pages/artical/artical.tsx
@@ -19,8 +19,6 @@ export default class artical extends Component {
         return <View className='at-article__h2'>{item.replace('##', '')}</View>
       } else if(item.indexOf('#') == 0){
         return <View className='at-article__h1'>{item.replace('#', '')}</View>
-      } else if(item.indexOf('img:') === 0){
-        return <Image className='at-article__img' src={item.replace('img:', '')} mode='widthFix' />
       } else if(item.indexOf('info:') == 0){
         return <View className='at-article__info'>{item.replace('info:', '')}</View>
       } else if(item.indexOf('p:') == 0){
@@ -29,12 +27,14 @@ export default class artical extends Component {
         return <View className='at-article__p'>· {item.replace('item:', '')}</View>
       } else if(item.indexOf('check:') == 0){
         return <View>□ {item.replace('check:', '')}</View>
+      } else if(item.indexOf('title:') == 0){
+        return <View className='article__h1'>{item.replace('title:', '')}</View>
+      } else if(item.indexOf('author:') == 0){
+        return <View className='at-article__info'>{item.replace('author:', '')}</View>
       } else if(item.indexOf('button:') == 0){
         return <Button className='button' open-type="contact" >{item.replace('button:', '')}</Button>
-      } else if(item.indexOf('title:') == 0){
-        return <View className='article__h2'>· {item.replace('title:', '')}</View>
-      } else if(item.indexOf('author:') == 0){
-        return <View className='at-article__info'>· {item.replace('author:', '')}</View>
+      } else if(item.indexOf('img:') === 0){
+        return <Image className='at-article__img' src={item.replace('img:', '')} mode='widthFix' />
       }
       return <View className='at-article__p'>{item}</View>
     })
